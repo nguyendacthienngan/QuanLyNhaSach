bb2f031ba73d6ec39e88bf5ea8b14d7598e709e8
Finn <53734879+FinnTruong@users.noreply.github.com>
2020-06-08 14:12:27 +0700

    Fix

--- BookStoreManagement/GUI/Forms/Form_Login.Designer.cs
+++ BookStoreManagement/GUI/Forms/Form_Login.Designer.cs
@@ -31,20 +31,21 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Login));
             this.gradientBackground1 = new GUI.GradientBackground();
-            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
-            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
-            this.Shadow = new System.Windows.Forms.PictureBox();
-            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             this.loginPanel = new System.Windows.Forms.Panel();
             this.slideUp_Login1 = new GUI.User_Controls.SlideUp_Login();
+            this.Shadow = new System.Windows.Forms.PictureBox();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             this.gradientBackground1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Shadow)).BeginInit();
             this.loginPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Shadow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             this.SuspendLayout();
             // 
             // gradientBackground1
             // 
+            this.gradientBackground1.Angle = 29F;
             this.gradientBackground1.Color0 = System.Drawing.Color.DarkViolet;
             this.gradientBackground1.Color1 = System.Drawing.Color.Orange;
             this.gradientBackground1.Controls.Add(this.loginPanel);
@@ -52,52 +53,10 @@
             this.gradientBackground1.Controls.Add(this.bunifuImageButton1);
             this.gradientBackground1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gradientBackground1.Location = new System.Drawing.Point(0, 0);
-            this.gradientBackground1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gradientBackground1.Margin = new System.Windows.Forms.Padding(2);
             this.gradientBackground1.Name = "gradientBackground1";
             this.gradientBackground1.Size = new System.Drawing.Size(548, 621);
             this.gradientBackground1.TabIndex = 0;
-            // 
-            // bunifuImageButton1
-            // 
-            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
-            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
-            this.bunifuImageButton1.ImageActive = null;
-            this.bunifuImageButton1.Location = new System.Drawing.Point(499, 2);
-            this.bunifuImageButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.bunifuImageButton1.Name = "bunifuImageButton1";
-            this.bunifuImageButton1.Size = new System.Drawing.Size(38, 46);
-            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.bunifuImageButton1.TabIndex = 7;
-            this.bunifuImageButton1.TabStop = false;
-            this.bunifuImageButton1.Zoom = 10;
-            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
-            // 
-            // bunifuDragControl1
-            // 
-            this.bunifuDragControl1.Fixed = true;
-            this.bunifuDragControl1.Horizontal = true;
-            this.bunifuDragControl1.TargetControl = this.gradientBackground1;
-            this.bunifuDragControl1.Vertical = true;
-            // 
-            // Shadow
-            // 
-            this.Shadow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Shadow.BackColor = System.Drawing.Color.Transparent;
-            this.Shadow.Enabled = false;
-            this.Shadow.Image = ((System.Drawing.Image)(resources.GetObject("Shadow.Image")));
-            this.Shadow.Location = new System.Drawing.Point(68, 44);
-            this.Shadow.Name = "Shadow";
-            this.Shadow.Size = new System.Drawing.Size(430, 556);
-            this.Shadow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Shadow.TabIndex = 11;
-            this.Shadow.TabStop = false;
-            // 
-            // bunifuElipse1
-            // 
-            this.bunifuElipse1.ElipseRadius = 35;
-            this.bunifuElipse1.TargetControl = this.loginPanel;
             // 
             // loginPanel
             // 
@@ -117,6 +76,48 @@
             this.slideUp_Login1.Size = new System.Drawing.Size(386, 519);
             this.slideUp_Login1.TabIndex = 0;
             // 
+            // Shadow
+            // 
+            this.Shadow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Shadow.BackColor = System.Drawing.Color.Transparent;
+            this.Shadow.Enabled = false;
+            this.Shadow.Image = ((System.Drawing.Image)(resources.GetObject("Shadow.Image")));
+            this.Shadow.Location = new System.Drawing.Point(68, 44);
+            this.Shadow.Name = "Shadow";
+            this.Shadow.Size = new System.Drawing.Size(430, 556);
+            this.Shadow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Shadow.TabIndex = 11;
+            this.Shadow.TabStop = false;
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(499, 2);
+            this.bunifuImageButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(38, 46);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 7;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.gradientBackground1;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 35;
+            this.bunifuElipse1.TargetControl = this.loginPanel;
+            // 
             // Form_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -124,13 +125,14 @@
             this.ClientSize = new System.Drawing.Size(548, 621);
             this.Controls.Add(this.gradientBackground1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form_Login";
             this.gradientBackground1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Shadow)).EndInit();
             this.loginPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Shadow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             this.ResumeLayout(false);
 
         }
