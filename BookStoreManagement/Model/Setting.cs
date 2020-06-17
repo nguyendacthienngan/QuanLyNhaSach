85fdc7bd6172f0d08b3c91f5b3904dc7b9720880
khanhchuong <chuongtangkhanh2104@gmail.com>
2020-06-17 11:46:01 +0700

    update model frontend

---
+++ BookStoreManagement/Model/Setting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Setting
+    {
+        public Setting(string key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        private string _key;
+
+        private string _value;
+
+        public string Key { get => _key; set => _key = value; }
+
+        public string Value { get => _value; set => _value = value; }
+    }
+}
