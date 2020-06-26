f92667291255086bc1bbf4ace1ac5278cac5d716
Nguyen Dac Thien Ngan <54399397+nguyendacthienngan@users.noreply.github.com>
2020-06-26 17:01:18 +0700

    Revert "Ui report"

--- BookStoreManagement/GUI/Program.cs
+++ BookStoreManagement/GUI/Program.cs
@@ -17,7 +17,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Main());
+            Application.Run(new Form_Login());
         }
     }
 }
