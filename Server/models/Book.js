1d6d84f65a5c60515672e4410914cf826c2248b2
khanhchuong <chuongtangkhanh2104@gmail.com>
2020-06-28 21:07:53 +0700

    delete book by id

--- Server/models/Book.js
+++ Server/models/Book.js
@@ -1,49 +1,56 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
   const options = {
     tableName: "Book", // QUAN TRỌNG!!
     comment: "",
-    indexes: []
+    indexes: [],
+    timestamps: true,
+    paranoid: true,
   };
-  const Book = sequelize.define('Book', {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Book = sequelize.define(
+    "Book",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stock: {
+        // Số lượng hàng còn trong kho
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      cost: {
+        // Giá mua
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      price: {
+        // Giá bán
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    author: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    stock: {
-      // Số lượng hàng còn trong kho
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    cost: {
-      // Giá mua
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    price: {
-      // Giá bán
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    type: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }
-  }, options);
+    options
+  );
   Book.associate = function (models) {
-      Book.belongsToMany(models.Order, {
-        through: 'OrderDetail',
-        foreignKey: 'bookID',
-        as: 'Orders'
-      })
-  }
+    Book.belongsToMany(models.Order, {
+      through: "OrderDetail",
+      as: "Orders",
+      foreignKey: "bookID",
+      otherKey: "orderID",
+    });
+  };
   return Book;
 };
--- Server/routes/book.route.js
+++ Server/routes/book.route.js
@@ -1,17 +1,15 @@
-const BookController = require('../controllers/book.controller');
-const express = require('express');
+const BookController = require("../controllers/book.controller");
+const express = require("express");
 const router = express.Router();
 
+router.get("/", BookController.getAllBooks);
+router.post("/search", BookController.searchBook);
 
-router.get('/', BookController.getAllBooks);
-router.post('/search', BookController.searchBook); 
+router.get("/get/count", BookController.getBookCount);
+router.get("/get/bestSeller", BookController.getBestSeller);
 
-router.get('/get/count', BookController.getBookCount);
-router.get('/get/bestSeller', BookController.getBestSeller);
-
-router.post('/add', BookController.addBook);
-router.post('/update', BookController.updateBook);
-router.post('/delete', BookController.deleteBook);
-
+router.post("/add", BookController.addBook);
+router.post("/update", BookController.updateBook);
+router.delete("/delete/:bookId", BookController.deleteBookById);
 
 module.exports = router;
