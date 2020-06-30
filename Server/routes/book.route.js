adef181b8a43a54d9b9b2a38addd4cbb62b0910e
khanhchuong <chuongtangkhanh2104@gmail.com>
2020-06-30 13:16:53 +0700

    find attributes for book

--- Server/routes/book.route.js
+++ Server/routes/book.route.js
@@ -3,13 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", BookController.getAllBooks);
-router.post("/search", BookController.searchBook);
+router.post("/search/:info", BookController.searchBookByInfo);
 
 router.get("/get/count", BookController.getBookCount);
 router.get("/get/bestSeller", BookController.getBestSeller);
 
 router.post("/add", BookController.addBook);
-router.post("/update", BookController.updateBook);
+router.put("/update", BookController.updateBook);
 router.delete("/delete/:bookId", BookController.deleteBookById);
 
 module.exports = router;
